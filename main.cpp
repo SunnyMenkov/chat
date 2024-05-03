7d29f37ecc2db9170980770f2ebfb671d3856651
Alexander Menkov <51756078+SunnyMenkov@users.noreply.github.com>
2024-05-03 18:43:13 +0300

    1.2 version of C++ Chat: - save previous history - time stamps of sessions in history.txt - /name command

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <ctime>
 
 #pragma comment (lib, "ws2_32.lib")
 
@@ -14,12 +15,13 @@ using namespace std;
 
 int main()
 {
+    time_t date = time(nullptr);
 
     ofstream out;
-    out.open("C:\\rep\\chat\\history.txt");
+    out.open("C:\\rep\\chat\\history.txt",ofstream::app);
     if (out.is_open())
     {
-        out << "history" << std::endl;
+        out << "history from " << asctime(localtime(&date)) << std::endl;
     }
 
     // Initialze winsock
@@ -57,6 +59,7 @@ int main()
     FD_ZERO(&master);
     FD_SET(listening, &master);
 
+    string users[999];
     while(true){
 
         fd_set copy = master;
@@ -65,14 +68,18 @@ int main()
 
         for (int i=0;i<socketCount;i++){
             SOCKET sock = copy.fd_array[i];
+
+            if (users[sock]=="") users[sock] = to_string(sock);
             if (sock == listening){
                 // принять соединение
                 SOCKET client = accept(listening, nullptr, nullptr);
-
+                //users[client] = to_string(client);
 
                 // add the new connection to the list of connected clients
                 FD_SET(client, &master); // самая важная часть, добавить сокет в листенинг
                 cout <<"!: " <<client <<endl;
+
+
 
                 // welcome message
                 string welcomeMsg = "Успешное подключение к серверу.\r\n";
@@ -85,8 +92,9 @@ int main()
                 // принять новое смс
                 int bytesIn = recv(sock,buf,4096,0);
                 if (out.is_open()){
-                    out <<sock <<": " << buf;
+                    out <<sock <<": " << buf<<std::endl;
                 }
+
 
                 if (bytesIn <= 0){
                     //drop the client
@@ -95,13 +103,28 @@ int main()
 
                 }
                 else{
+                    //команды
+                    if (buf[0] == '/')
+                    {
+                        // Is the command quit?
+                        string cmd = string(buf, bytesIn);
+                        if (cmd.substr(0,5) == "/name")
+                        {
+                          //
+                          users[sock] = cmd.substr(6,cmd.length()-6);
+                        }
+
+                        // Unknown command
+                        continue;
+                    }
+
                     //отправляем смс другим клиентам, проверяем что не на листенинг сокет
                     for (int i=0;i<master.fd_count;i++){
                         SOCKET outSock = master.fd_array[i];
                         if (outSock!= listening && outSock!= sock){
 
                             ostringstream ss;
-                            ss << "SOCKET #" << sock << ":" << buf << "\r\n";
+                            ss  << users[sock] << ":" << buf << "\r\n";
                             string strOut = ss.str();
 
 

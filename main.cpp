8d948ba71fe61a1b7dc52e8d7c3fdfaccc9b84ea
Alexander Menkov <51756078+SunnyMenkov@users.noreply.github.com>
2024-05-03 14:02:43 +0300

    First version of C++ Chat: - работа клиентов через putty - правильный импорт библиотек

---
+++ main.cpp
@@ -0,0 +1,111 @@
+//
+// Created by amenk on 03.05.2024.
+//
+
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+    // Initialze winsock
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+
+    int wsOk = WSAStartup(ver, &wsData);
+    if (wsOk != 0)
+    {
+        cerr << "Can't Initialize winsock! Quitting" << endl;
+        return 0;
+    }
+
+    // Create a socket
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == INVALID_SOCKET)
+    {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return 0;
+    }
+
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton ....
+
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+    // Tell Winsock the socket is for listening
+    listen(listening, SOMAXCONN);
+
+    // fd_set - структура сокета в С++
+    fd_set master;
+    FD_ZERO(&master);
+    FD_SET(listening, &master);
+
+    while(true){
+
+        fd_set copy = master;
+
+        int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+        for (int i=0;i<socketCount;i++){
+            SOCKET sock = copy.fd_array[i];
+            if (sock == listening){
+                // принять соединение
+                SOCKET client = accept(listening, nullptr, nullptr);
+
+                // add the new connection to the list of connected clients
+                FD_SET(client, &master); // самая важная часть, добавить сокет в листенинг
+                // welcome message
+                string welcomeMsg = "Hello Egor B.";
+                send(client, welcomeMsg.c_str(),welcomeMsg.length()+1,0);
+            }
+            else {
+                char buf[4096];
+                ZeroMemory(buf, sizeof(buf)); //буфер
+
+                // принять новое смс
+                int bytesIn = recv(sock,buf,4096,0);
+                if (bytesIn <= 0){
+                    //drop the client
+                    closesocket(sock);
+                    FD_CLR(sock, &master);
+
+                }
+                else{
+                    //отправляем смс другим клиентам, проверяем что не на листенинг сокет
+                    for (int i=0;i<master.fd_count;i++){
+                        SOCKET outSock = master.fd_array[i];
+
+                        if (outSock!= listening && outSock!= sock){
+                            send(outSock, buf, bytesIn+1, 0);
+                        }
+                    }
+
+                }
+
+
+            }
+
+        }
+
+
+    }
+
+    // Add our first socket that we're interested in interacting with; the listening socket!
+    // It's important that this socket is added for our server or else we won't 'hear' incoming
+    // connections
+
+
+
+    // Cleanup winsock
+    WSACleanup();
+
+    system("pause");
+}

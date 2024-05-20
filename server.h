22f6a2396c8ce959636b8e273f1fa2c9eb3915e2
Alexander Menkov <51756078+SunnyMenkov@users.noreply.github.com>
2024-05-20 21:08:32 +0300

    edited server.h

--- server.h
+++ server.h
@@ -2,6 +2,8 @@
 // Created by Alexander on 15.05.2024.
 //
 #include <WS2tcpip.h>
+#include <string>
+using namespace std;
 #ifndef CHAT_SERVER_H
 #define CHAT_SERVER_H
 
@@ -10,4 +12,5 @@
 
 int server();
 void send_message(fd_set master,SOCKET listening,SOCKET sock,char buf[]);
+string user_login(string cmd, string login, string password);
 #endif //CHAT_SERVER_H
